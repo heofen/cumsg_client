11b5475b886961a65564bf0001a9900582d048ad
heofen <heofen@heofen.cum>
2024-10-30 22:01:44 +0300

    hui

--- main.py
+++ main.py
@@ -103,4 +103,4 @@ def handle_receive_message(data):
 
 if __name__ == '__main__':
     asyncio.run(grpc_client())
-    socketio.run(app, host='0.0.0.0', port=50053, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', port=5003, allow_unsafe_werkzeug=True)
